abb8f3761603d3ea8f7e88044129a7fc5b00cbe1
Cheena <97012270+Nezogoku@users.noreply.github.com>
2023-03-16 11:15:14 -0400

    Delete ipk directory

--- ipk/lzss/lzss.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef BLINXLZSS_HPP
-#define BLINXLZSS_HPP
-
-//Slightly edited code from the Okumura LZSS (de)compressor on segaretro
-void decompressLZSS(unsigned char *in, int in_size, unsigned char *&out, int out_size);
-
-#endif
