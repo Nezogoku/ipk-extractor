61a7c1c7171bacc273dac1a82db5f7cb73602cc5
Cheena <97012270+Nezogoku@users.noreply.github.com>
2023-03-16 13:26:41 -0400

    Update to_ipk.cpp

--- ipk/to_ipk.cpp
+++ ipk/to_ipk.cpp
@@ -2,63 +2,10 @@
 #include <cstdio>
 #include <string>
 #include "directory.hpp"
+#include "stringstream.hpp"
 #include "bit_byte.hpp"
 #include "lzss/lzss.hpp"
 #include "ipk.hpp"
-
-//Struct for custom stringstream
-struct sstream {
-    uchar *stream;
-    unsigned strsize = 0, spos = 0, ssize = 0;
-
-    std::string ssub; unsigned isub;
-};
-
-//Code for custom stringstream
-int getStream(sstream &str) {
-    int ret = 0;
-    bool quotes = 0;
-
-    str.ssub = "";
-    str.isub = -1;
-
-    if (!str.stream) return ret;
-
-    str.spos += str.ssize;
-    while (true) {
-        if (str.spos >= str.strsize) break;
-
-        if (str.stream[str.spos] == '\"') { quotes = !quotes; str.spos += 1; }
-        else if (isgraph(str.stream[str.spos])) break;
-        else str.spos += 1;
-    }
-
-    str.ssize = 0;
-    while (true) {
-        if (str.spos + str.ssize >= str.strsize) break;
-
-        if (str.stream[str.spos + str.ssize] == '\"') { quotes = !quotes; str.ssize += 1; }
-        else if (isspace(str.stream[str.spos + str.ssize]) && !quotes) break;
-        else str.ssize += 1;
-    }
-
-    for (int c = 0; c < str.ssize; ++c) {
-        if (str.stream[str.spos + c] == '\"') continue;
-        else str.ssub += str.stream[str.spos + c];
-    }
-
-    if (!str.ssub.empty()) {
-        bool isnum = true;
-        for (auto &c : str.ssub) if (!isdigit(c)) { isnum = false; break; }
-        if (isnum) { str.isub = stol(str.ssub, 0, 10); str.ssub.clear(); }
-
-        ret = 1;
-    }
-    else { delete[] str.stream; str.stream = 0; }
-
-    return ret;
-}
-
 
 int toIPK(sstream &str, std::string iroot, unsigned ialign, unsigned ientries, unsigned &isize, unsigned level, uchar *out) {
     const unsigned IPK1 = 0x49504B31;
