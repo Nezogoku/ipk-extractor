9a47f8f1d2127c2b796bf3b3c271fd5749a63bbb
Cheena <97012270+Nezogoku@users.noreply.github.com>
2024-01-30 15:02:41 -0500

    Remove lzss files

--- ipk/lzss/lzss.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef BLINXLZSS_HPP
-#define BLINXLZSS_HPP
-
-//Slightly edited code from the Okumura LZSS (de)compressor on segaretro
-void compressLZSS(unsigned char *in, unsigned in_size, unsigned char *out, unsigned &out_size);
-void decompressLZSS(unsigned char *in, unsigned in_size, unsigned char *out, unsigned out_size);
-
-#endif
